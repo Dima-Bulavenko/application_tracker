5ff3a0c6004cc20da463ce59470601c0a5eb238f
Dima Bulavenko <dima99bylovenko@gmail.com>
2024-12-28 22:55:49 +0000

    feat: Create dependency to get session in fastapi routes.

--- backend/app/dependencies.py
+++ backend/app/dependencies.py
@@ -6,9 +6,10 @@ from sqlalchemy.orm import Session
 from .db import Session as SessionMaker
 
 
-def get_session():
+def get_session() -> Session:
     with SessionMaker() as session:
         yield session
+        session.commit()
 
 
 SessionDep = Annotated[Session, Depends(get_session)]
