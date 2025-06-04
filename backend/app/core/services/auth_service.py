7f52c384a03b0f83dab581bf2704dfd6614f535f
Dima Bulavenko <dima99bylovenko@gmail.com>
2025-06-04 20:29:07 +0000

    feat: Change "authenticate()" method of "AuthService" class so that it retruns model of authenticated user and not create access and refresh tokens.

--- backend/app/core/services/auth_service.py
+++ backend/app/core/services/auth_service.py
@@ -1,7 +1,7 @@
-from app.core.dto import Tokens
+from app.core.dto import UserRead
 from app.core.exceptions import InvalidPasswordError, UserNotFoundError
 from app.core.repositories import IUserRepository
-from app.core.security import IPasswordHasher, ITokenProvider
+from app.core.security import IPasswordHasher
 
 
 class AuthService:
@@ -9,20 +9,15 @@ class AuthService:
         self,
         user_repo: IUserRepository,
         password_hasher: IPasswordHasher,
-        token_provider: ITokenProvider,
     ) -> None:
         self.user_repo = user_repo
         self.password_hasher = password_hasher
-        self.token_provider = token_provider
 
-    async def authenticate(self, email: str, password) -> Tokens:
+    async def authenticate(self, email: str, password) -> UserRead:
         user = await self.user_repo.get_by_email(email)
         # TODO: Check is user active to
         if not user:
             raise UserNotFoundError(f"User with {email} does not exist")  # noqa: EM102
         if not self.password_hasher.verify(password, user.password):
             raise InvalidPasswordError("Incorrect password")
-
-        access_token = self.token_provider.create_access_token(user)
-        refresh_token = self.token_provider.create_refresh_token(user)
-        return Tokens(access=access_token, refresh=refresh_token)
+        return UserRead.model_validate(user)
