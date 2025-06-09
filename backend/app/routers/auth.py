bfccdc09ff90181b54ada3d1748b2cbdff3a7b6a
Dima Bulavenko <dima99bylovenko@gmail.com>
2025-06-09 22:33:47 +0000

    feat: Create "login" route.

--- backend/app/routers/auth.py
+++ backend/app/routers/auth.py
@@ -1,17 +1,15 @@
-from typing import Annotated
-
-from fastapi import APIRouter, Form, Response, status
+from fastapi import APIRouter, Response, status
 
 from app import Tags
 from app.core.dto import Token
-from app.dependencies import AuthenticatedUserDep
+from app.dependencies import LoginUserDep
 from app.utils import set_refresh_token
 
 router = APIRouter(prefix="/auth", tags=[Tags.AUTHENTICATION])
 
 
 @router.post("/login", status_code=status.HTTP_200_OK)
-async def login(tokens: AuthenticatedUserDep, response: Response) -> Token:
+async def login(tokens: LoginUserDep, response: Response) -> Token:
     """
     **Create** a user with provided email and password.
 
@@ -19,5 +17,6 @@ async def login(tokens: AuthenticatedUserDep, response: Response) -> Token:
 
     If the user with the email exist, return success response but send warning email to the email address.
     """
-    set_refresh_token(response, tokens.refresh)
-    return Token(token=tokens.access)
+
+    set_refresh_token(response, tokens.refresh.token)
+    return tokens.access
