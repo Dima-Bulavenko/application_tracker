bfcb5d7b04331f251839de780b08eecec861777e
Dima Bulavenko <dima99bylovenko@gmail.com>
2025-06-09 15:26:48 +0000

    feat: Delete "authenticate()" method of AuthService and create new "login_with_credentials()".

--- backend/app/core/services/auth_service.py
+++ backend/app/core/services/auth_service.py
@@ -1,7 +1,7 @@
-from app.core.dto import UserRead
+from app.core.dto import AuthTokenPair, UserLogin
 from app.core.exceptions import InvalidPasswordError, UserNotFoundError
 from app.core.repositories import IUserRepository
-from app.core.security import IPasswordHasher
+from app.core.security import IPasswordHasher, ITokenProvider
 
 
 class AuthService:
@@ -9,15 +9,20 @@ class AuthService:
         self,
         user_repo: IUserRepository,
         password_hasher: IPasswordHasher,
+        token_provider: ITokenProvider,
     ) -> None:
         self.user_repo = user_repo
         self.password_hasher = password_hasher
+        self.token_provider = token_provider
 
-    async def authenticate(self, email: str, password) -> UserRead:
-        user = await self.user_repo.get_by_email(email)
+    async def login_with_credentials(self, user_creds: UserLogin) -> AuthTokenPair:
+        user = await self.user_repo.get_by_email(user_creds.email)
         # TODO: Check is user active to
         if not user:
-            raise UserNotFoundError(f"User with {email} does not exist")  # noqa: EM102
-        if not self.password_hasher.verify(password, user.password):
+            raise UserNotFoundError(f"User with {user_creds.email} does not exist")  # noqa: EM102
+        if not self.password_hasher.verify(user_creds.password, user.password):
             raise InvalidPasswordError("Incorrect password")
-        return UserRead.model_validate(user)
+
+        access_token = self.token_provider.create_refresh_token(user)
+        refresh_token = self.token_provider.create_refresh_token(user)
+        return AuthTokenPair(access=access_token, refresh=refresh_token)
