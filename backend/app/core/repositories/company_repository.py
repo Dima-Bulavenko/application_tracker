916f8e03c4d37921c61dde9ee3c3b394e2a26b7b
Dima Bulavenko <dima99bylovenko@gmail.com>
2025-07-01 01:18:47 +0000

    feat: Create interface for company repository.

---
+++ backend/app/core/repositories/company_repository.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+
+from app.core.domain import Company
+
+
+class ICompanyRepository(ABC):
+    @abstractmethod
+    async def create(self, company: Company) -> Company: ...
+
+    @abstractmethod
+    async def get_by_name(self, name: str) -> Company | None: ...
+
+    @abstractmethod
+    async def get_companies(
+        self,
+        limit: int | None = None,
+        offset: int | None = None,
+    ) -> list[Company]: ...
