206b47d1e16741ae6c55bdf015d33ab0ce8d3273
Dima Bulavenko <dima99bylovenko@gmail.com>
2025-07-01 01:16:02 +0000

    feat: Create company domain model.

--- backend/app/core/domain/__init__.py
+++ backend/app/core/domain/__init__.py
@@ -1,4 +1,5 @@
 from .application import Application, AppStatus, WorkLocation, WorkType
+from .company import Company
 from .user import User
 
-__all__ = ["AppStatus", "Application", "User", "WorkLocation", "WorkType"]
+__all__ = ["AppStatus", "Application", "Company", "User", "WorkLocation", "WorkType"]
