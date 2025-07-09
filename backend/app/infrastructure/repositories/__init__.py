4553c3ae0737fe0cb9140b3da3b867cc0a07b163
Dima Bulavenko <dima99bylovenko@gmail.com>
2025-07-09 00:59:48 +0000

    feat: Implemnt "ICompanyRepository" interface.

--- backend/app/infrastructure/repositories/__init__.py
+++ backend/app/infrastructure/repositories/__init__.py
@@ -1,4 +1,9 @@
 from .slq_alchemy.application import ApplicationSQLAlchemyRepository
+from .slq_alchemy.company import CompanySQLAlchemyRepository
 from .slq_alchemy.user import UserSQLAlchemyRepository
 
-__all__ = ["ApplicationSQLAlchemyRepository", "UserSQLAlchemyRepository"]
+__all__ = [
+    "ApplicationSQLAlchemyRepository",
+    "CompanySQLAlchemyRepository",
+    "UserSQLAlchemyRepository",
+]
---
+++ backend/app/infrastructure/repositories/slq_alchemy/company.py
@@ -0,0 +1,39 @@
+from __future__ import annotations
+
+from sqlalchemy import select
+
+from app.core.domain import Company
+from app.core.repositories import ICompanyRepository
+from app.db.models import Company as CompanyModel
+
+from .config import SQLAlchemyRepository
+
+
+class CompanySQLAlchemyRepository(
+    SQLAlchemyRepository[CompanyModel], ICompanyRepository
+):
+    model = CompanyModel
+
+    async def create(self, company: Company) -> Company:
+        company_model = self.model(**company.model_dump())
+        self.session.add(company_model)
+        await self.session.flush()
+        return Company.model_validate(company_model, from_attributes=True)
+
+    async def get_by_name(self, name: str) -> Company | None:
+        statement = select(self.model).where(self.model.name == name)
+        company_model = await self.session.scalar(statement)
+        return (
+            Company.model_validate(company_model, from_attributes=True)
+            if company_model
+            else None
+        )
+
+    async def get_companies(
+        self,
+        limit: int | None = None,
+        offset: int | None = None,
+    ) -> list[Company]:
+        statement = select(self.model).limit(limit).offset(offset)
+        company_model = await self.session.scalars(statement)
+        return [Company.model_validate(c, from_attributes=True) for c in company_model]
