23afbc34cd0a71bc466a135093116d9855bf22f4
Dima Bulavenko <dima99bylovenko@gmail.com>
2025-07-22 19:58:44 +0000

    feat: Create endpoint to appdate application

--- backend/app/routers/application.py
+++ backend/app/routers/application.py
@@ -1,16 +1,16 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status
 
 from app import Tags
-from app.core.dto import ApplicationCreate, ApplicationRead
+from app.base_schemas import ErrorResponse
+from app.core.dto import ApplicationCreate, ApplicationRead, ApplicationUpdate
+from app.core.exceptions import ApplicationNotFoundError, UserNotAuthorizedError
 from app.dependencies import ActiveUserDep, ApplicationServiceDep
 
 router = APIRouter(prefix="/applications", tags=[Tags.APPLICATION])
 
 
 @router.get("/")
-async def get_applications_by_email(
-    app_service: ApplicationServiceDep, user: ActiveUserDep
-) -> list[ApplicationRead]:
+async def get_applications_by_email(app_service: ApplicationServiceDep, user: ActiveUserDep) -> list[ApplicationRead]:
     apps = await app_service.get_applications_by_user_email(user.email)
     return apps
 
@@ -20,4 +20,30 @@ async def create_application(
     app_service: ApplicationServiceDep, app: ApplicationCreate, user: ActiveUserDep
 ) -> ApplicationRead:
     application = await app_service.create(app, user.id)
+    return application
+
+
+@router.patch(
+    "/{application_id}",
+    responses={
+        status.HTTP_401_UNAUTHORIZED: {"description": "Access token is invalid", "model": ErrorResponse},
+        status.HTTP_403_FORBIDDEN: {
+            "description": "User is not authorized to update this application, or is not active",
+            "model": ErrorResponse,
+        },
+        status.HTTP_404_NOT_FOUND: {"description": "Application or User not found", "model": ErrorResponse},
+    },
+)
+async def update_application(
+    application_id: int,
+    app_service: ApplicationServiceDep,
+    app: ApplicationUpdate,
+    user: ActiveUserDep,
+) -> ApplicationRead:
+    try:
+        application = await app_service.update(application_id, app, user.id)
+    except ApplicationNotFoundError as e:
+        raise HTTPException(status_code=404, detail=str(e))
+    except UserNotAuthorizedError as e:
+        raise HTTPException(status_code=403, detail=str(e))
     return application
