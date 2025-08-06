98d2ec08a6e4c3fb0663aa37233abfd86ed5c9f1
Dima Bulavenko <dima99bylovenko@gmail.com>
2025-08-06 20:17:23 +0000

    refactor: Consolidate import statements for improved readability and organization

--- backend/app/infrastructure/security/__init__.py
+++ backend/app/infrastructure/security/__init__.py
@@ -1,10 +1,6 @@
 from .password_hasher import PasslibHasher as PasslibHasher
 from .token_provider import (
     AccessTokenStrategy as AccessTokenStrategy,
-)
-from .token_provider import (
     RefreshTokenStrategy as RefreshTokenStrategy,
-)
-from .token_provider import (
     VerificationTokenStrategy as VerificationTokenStrategy,
 )
