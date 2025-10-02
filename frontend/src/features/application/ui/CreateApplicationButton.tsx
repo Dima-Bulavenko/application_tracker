28a059d69bf55184cd1fe01996ae126337471f14
Dima Bulavenko <dima99bylovenko@gmail.com>
2025-10-02 21:30:13 +0000

    feat: Replace CreateApplicationButton with IconButton for improved UI and fixed positioning

--- frontend/src/features/application/ui/CreateApplicationButton.tsx
+++ frontend/src/features/application/ui/CreateApplicationButton.tsx
@@ -1,16 +1,24 @@
 import { Add as AddIcon } from '@mui/icons-material';
-import { Button, type ButtonProps } from '@mui/material';
+import { Box, IconButton, type ButtonProps } from '@mui/material';
 
 type CreateApplicationButtonProps = ButtonProps;
 
 export function CreateApplicationButton(props: CreateApplicationButtonProps) {
   return (
-    <Button
-      startIcon={<AddIcon />}
-      variant='contained'
-      color='primary'
-      {...props}>
-      New Application
-    </Button>
+    <Box
+      sx={(theme) => ({
+        position: 'fixed',
+        bottom: theme.spacing(12),
+        right: theme.spacing(2),
+        zIndex: theme.zIndex.appBar,
+      })}>
+      <IconButton
+        sx={{ backgroundColor: 'primary.main' }}
+        variant='contained'
+        size='medium'
+        {...props}>
+        <AddIcon fontSize='inherit' />
+      </IconButton>
+    </Box>
   );
 }
