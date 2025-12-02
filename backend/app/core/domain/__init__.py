c9ec69672e8e95de8e95366cc227063bf8d51b12
Dima Bulavenko <dima99bylovenko@gmail.com>
2025-12-02 16:37:14 +0000

    feat(user): add OAuthProvider and update User model with oauth fields

--- backend/app/core/domain/__init__.py
+++ backend/app/core/domain/__init__.py
@@ -6,5 +6,5 @@ from .application import (
 )
 from .company import Company as Company
 from .refresh_token import RefreshToken as RefreshToken
-from .user import User as User
+from .user import OAuthProvider as OAuthProvider, User as User
 from .verification_token import VerificationToken as VerificationToken
--- backend/app/db/models/user.py
+++ backend/app/db/models/user.py
@@ -2,8 +2,10 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from sqlalchemy import String, sql
+from sqlalchemy import String, UniqueConstraint, sql
 from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from app.core.domain import OAuthProvider
 
 from .config import Base, pk_tp, time_create_tp, time_update_tp
 
@@ -13,9 +15,13 @@ if TYPE_CHECKING:
 
 class User(Base):
     __tablename__ = "user"
+    __table_args__ = (UniqueConstraint("oauth_provider", "oauth_id", name="uq_oauth_provider_id"),)
+
     id: Mapped[pk_tp]
     email: Mapped[str] = mapped_column(unique=True)
-    password: Mapped[str]
+    password: Mapped[str | None]
+    oauth_provider: Mapped[OAuthProvider] = mapped_column(server_default=OAuthProvider.LOCAL.value)
+    oauth_id: Mapped[str | None] = mapped_column(String(255))
     first_name: Mapped[str | None] = mapped_column(String(40))
     second_name: Mapped[str | None] = mapped_column(String(40))
     time_create: Mapped[time_create_tp]
